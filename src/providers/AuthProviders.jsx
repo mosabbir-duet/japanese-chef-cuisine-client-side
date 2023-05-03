b3ad957eba16db96713d0430f701e704504ee44f
mosabbir.duet <mosabbir.duet@gmail.com>
2023-05-03 21:03:46 +0600

    Added firebase.config and also used Environment varaible for privacy

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,13 +1,17 @@
+import { getAuth } from "firebase/auth";
 import React, { createContext } from "react";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
+const auth = getAuth(app);
+
 const AuthProviders = ({ children }) => {
-  const contextInfo = {
+  const authInfo = {
     displayName: "Sabbir",
   };
   return (
-    <AuthContext.Provider value={contextInfo}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
